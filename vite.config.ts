a39f68090b2e688710f95766263a89cf27904365
cynthia.costa <cynthia.costa@quintoandar.com.br>
2024-08-08 21:54:03 -0300

    fix: route

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  base: '/',
   plugins: [react(), svgr({ include: '**/*.svg' })],
 });
