956c82bf4be0fefc83ac621ff8f92f137e9192b1
cynthia.costa <cynthia.costa@quintoandar.com.br>
2024-09-24 09:34:14 -0300

    refactor: apply bento grid on mobile

--- src/data/codeData.ts
+++ src/data/codeData.ts
@@ -8,6 +8,10 @@ export const codeData: { src: string; url: string }[] = [
     url: 'https://codepen.io/cycosta/pen/ZEQXvJW',
   },
   {
+    src: '/images/projects/big-white-cloud.webm',
+    url: 'https://cycosta.dev/bigwhitecloud',
+  },
+  {
     src: '/images/projects/login-sign-up.webm',
     url: 'https://codepen.io/cycosta/pen/GRoePzE?editors=1100',
   },
@@ -18,9 +22,5 @@ export const codeData: { src: string; url: string }[] = [
   {
     src: '/images/projects/animated-solar-system.webm',
     url: 'https://codepen.io/cycosta/pen/OJMebrQ',
-  },
-  {
-    src: '/images/projects/big-white-cloud.webm',
-    url: 'https://cycosta.dev/bigwhitecloud',
   },
 ];
